59d83642d071f112af7f7b99c95ce7658e834860
Guilherme-XCR <guilhermerodrigues.0721@gmail.com>
2023-12-15 08:51:59 -0300

    Quinto Projeto - SideBar

--- 05 - Sidebar/script.js
+++ 05 - Sidebar/script.js
@@ -1,1 +1,17 @@
+const openBtn = document.getElementById("openBtn")
+const closeBtn = document.getElementById("closeBtn")
 
+openBtn.addEventListener("click", () => {openNav()});
+closeBtn.addEventListener("click", () => {closeNav()});
+
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("content").style.marginLeft = "250px";
+    document.getElementById("openBtn").style.display = "none";
+  }
+
+  function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("content").style.marginLeft = "0";
+    document.getElementById("openBtn").style.display = "block";
+  }
