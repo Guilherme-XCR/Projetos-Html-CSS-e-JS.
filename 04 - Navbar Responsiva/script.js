da0f1a02f043d960e338dd4d468fb40a990f2e4d
Guilherme-XCR <guilhermerodrigues.0721@gmail.com>
2023-12-15 08:28:21 -0300

    Quarto Projeto - Uma NavBar Responsiva.

--- 04 - Navbar Responsiva/script.js
+++ 04 - Navbar Responsiva/script.js
@@ -1,1 +1,8 @@
-
+function hiddenFuction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
